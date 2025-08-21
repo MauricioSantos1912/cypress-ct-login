c84d047cf5a27caec184439165c200b8156cba10
MauricioSantos1912 <zeusbetinho123@gmail.com>
2025-08-21 17:44:54 -0300

    Firt commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,20 @@
+describe('template spec', () => {
+  it('ct_login__successful', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module')
+  
+  })
+    it('ct_login__Failed', () => {
+     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+     cy.get("[name='username']").type('Admin1231')
+     cy.get("[name='password']").type('admin123')
+     cy.get("[type='submit']").click()
+     cy.get('.oxd-alert')
+
+
+  }) 
+})
